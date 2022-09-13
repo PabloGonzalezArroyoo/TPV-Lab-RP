11ea4794053e2049429c78c4227d6b62f308f5cb
Pavipollo17 <pagonz30@ucm.es>
2022-09-13 18:52:46 +0200

    Progreso 13-09-2022

--- LabEjercicio1/LabEjercicio1.cpp
+++ LabEjercicio1/LabEjercicio1.cpp
@@ -1,19 +1,117 @@
-// LabEjercicio1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// LabEjercicio1.cpp : Pablo González Arroyo y Rafael Argandoña Blácido
 
 #include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct Coche {
+    int code;
+    int price;
+    string model;
+};
+
+struct ListaCoches {
+    Coche* elems; // Array dinámico
+    int cap;
+    int cont;
+};
+
+struct Alquiler {
+    Coche* car;
+    string date;
+    int days;
+};
+
+struct ListaAlquileres {
+    Alquiler* elems;
+    int cap;
+    int cont;
+};
+
+bool cargarCoches(ListaCoches& lc) {
+    ifstream in;
+    in.open("coches.txt");
+    if (!in.is_open()) return false;
+    else {
+        int n;
+        in >> n;
+        lc.elems = new Coche[n];
+        lc.cont = n;
+        lc.cap = lc.cont + 10;
+
+        int i = 0;
+        while (i < n) {
+            in >> lc.elems[i].code;
+            in >> lc.elems[i].price;
+            in.get();
+            getline(in, lc.elems[i].model);
+            cout << lc.elems[i].code << " " << lc.elems[i].price << " " << lc.elems[i].model << endl;
+            i++;
+        }
+
+        in.close();
+        return true;
+    }
+}
+
+bool leerAlquileres(ListaAlquileres& la, ListaCoches lc) {
+    ifstream in;
+    in.open("rent.txt");
+    if (!in.is_open()) return false;
+    else {
+        int n;
+        in >> n;
+        la.elems = new Alquiler[n];
+        la.cont = n;
+        la.cap = la.cont + 10;
+
+        int i = 0;
+        while (i < n) {
+            int code;
+            in >> code;
+            la.elems[i].car = buscarCoche(lc, code);
+
+            in >> la.elems[i].car;
+            in.get();
+            getline(in, lc.elems[i].model);
+            cout << lc.elems[i].code << " " << lc.elems[i].price << " " << lc.elems[i].model << endl;
+            i++;
+        }
+
+        in.close();
+        return true;
+    }
+}
+
+Coche* buscarCoche(ListaCoches lc, int code) {
+    Coche* car = nullptr;
+    
+    int i = 0;
+    bool found = false;
+    while (!found && i < lc.cont) {
+        if (lc.elems[i].code == code) {
+            car = &lc.elems[i];
+            found = true;
+        }
+        else i++;
+    }
+
+    return car;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Inicialización de la lista de coches
+    ListaCoches lc;
+    bool readLc = cargarCoches(lc);
+    if (readLc) cout << "\nLEIDO COCHES.TXT\n";
+    else cout << "\nNO LEIDO COCHES.TXT\n";
+
+    // Inicialización de la lista de alquileres
+    ListaAlquileres la;
+    bool readLa = leerAlquileres(la);
+    if (readLa) cout << "\nLEIDO RENT.TXT\n";
+    else cout << "\nNO LEIDO RENT.TXT\n";
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
