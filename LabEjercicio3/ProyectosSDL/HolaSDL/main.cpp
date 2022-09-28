f27abaa94d9e6fac36ce9b6d84bc419ec9f8c80a
Pavipollo17 <pagonz30@ucm.es>
2022-09-28 11:32:18 +0200

    SDL 27/09

--- LabEjercicio3/ProyectosSDL/HolaSDL/main.cpp
+++ LabEjercicio3/ProyectosSDL/HolaSDL/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 using uint = unsigned int;
 
 void firstTest() {
+	// Inicialización
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
@@ -18,12 +19,20 @@ void firstTest() {
 	window = SDL_CreateWindow("First test with SDL", SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED, winWidth, winHeight, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	// Textura
+	SDL_Texture* texture; // Variable para la textura
+	string filename = "../images/background1.png"; // Nombre del fichero con la imagen
+	SDL_Surface* surface = IMG_Load(filename.c_str());
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface); // Se borra la estructura auxiliar
+
+	// Renderizado
 	if (window == nullptr || renderer == nullptr)
 		cout << "Error cargando SDL" << endl;
 	else {
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
+		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+		SDL_RenderPresent(renderer); // Muestra la escena
 		SDL_Delay(5000);
 	}
 	SDL_DestroyRenderer(renderer);
