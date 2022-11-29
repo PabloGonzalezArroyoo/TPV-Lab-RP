43d11f71ad3cb6bab8c7119fb6a16eaac37449f4
Pavipollo17 <pagonz30@ucm.es>
2022-11-29 18:40:15 +0100

    EXCEPCIONES

--- Práctica 2/ProyectosSDL/HolaSDL/Exceptions.h
+++ Práctica 2/ProyectosSDL/HolaSDL/Exceptions.h
@@ -6,21 +6,21 @@ using namespace std;
 //ArkanoidError
 class ArkanoidError : public logic_error {
 public:
-	explicit ArkanoidError(const string& message);
-	virtual const char* what() const throw();
+	ArkanoidError(const string& message) : logic_error(message) {};
 };
 
 // SDL_Error
 class SDL_Error : public ArkanoidError {
-	// SDL GetError, IMG GetError y TTF GetError
+	SDL_Error(const string& message) : ArkanoidError("SDL_ERROR: " + message) {};
+	// SDL GetError, IMG GetError
 };
 
 // FileNotFoundError
 class FileNotFoundError : public ArkanoidError {
-
+	FileNotFoundError(const string& message) : FileNotFoundError("FileNotFoundError: " + message) {};
 };
 
 // FileFormatError
 class FileFormatError : public ArkanoidError {
-
+	FileN
 };
